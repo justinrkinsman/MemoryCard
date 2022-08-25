c26e6f2d175492ab7ef6c402ca7bd4deabf641af
Justin <justinrkinsman@gmail.com>
2022-08-25 10:39:19 -0300

    Add component file for Score.js

---
+++ memory-card/src/components/Score.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function ScoreComponent() {
+    //const [state, setState] = useState(default state)
+
+    //const changeState = () => {
+    //  setState(new state) 
+    //}
+}
