a2ee974dd82b4ee9857f93d9e69f5ed4addd3b80
Justin <justinrkinsman@gmail.com>
2022-08-26 10:47:22 -0300

    Create function to ranomly populate gameboard with images

--- memory-card/src/components/Gameboard.js
+++ memory-card/src/components/Gameboard.js
@@ -18,28 +18,60 @@ import xeno from "../images/xenomorph.jpg"
 import predator from "../images/predator.jpg"
 import pinhead from "../images/pinhead.jpg"
 
+const images = [
+    ['Dracula', dracula], 
+    ['Frankenstein', frankenstein], 
+    ['Nosferatu', nosferatu],
+    ['The Phantom of the Opera', phantom], 
+    ['The Mummy', mummy],
+    ['The Invisible Man', invisible], 
+    ['The Wolf Man', wolfman],
+    ['The Creature From the Black Lagoon', creature], 
+    ['Jason Voorhees', jason],
+    ['Freddy Krueger', freddy], 
+    ['Michael Myers', michael], 
+    ['Chucky', chucky],
+    ['Leatherface', leather], 
+    ['Xenomorph', xeno],
+    ['Predator', predator], 
+    ['Pinhead', pinhead]
+]
+
 export function Gameboard() {
-{/*put all the images in an array and have a function randomly select from the array and remove each one as it selects it */}
+
+
+    const RandomImagesArray = () => {
+        let currentIndex = images.length
+        while (currentIndex !== 0){
+            let randomIndex = Math.floor(Math.random() * currentIndex)
+            currentIndex--
+
+            [images[currentIndex], images[randomIndex]] = [images[randomIndex], images[currentIndex]]
+        }
+        return images
+    }
+
+    RandomImagesArray()
 
     return(
         <>
-        <fieldset className="gameboard"> {/*Create function that will randomly assign src and one that will randomly assign alt. Put those functions in the props  */}
-            <Card source={dracula} alternate='Dracula'/>   
-            <Card source={frankenstein} alternate='Frankenstein'/>
-            <Card source={nosferatu} alternate='Nosferatu'/>
-            <Card source={phantom} alternate='The Phantom of the Opera'/>
-            <Card source={mummy} alternate="The Mummy"/>
-            <Card source={invisible} alternate="The Invisible Man"/>
-            <Card source={wolfman} alternate="The Wolf Man"/>
-            <Card source={creature} alternate="The Creature From the Black Lagoon"/>
-            <Card source={jason} alternate="Jason Voorhees"/>
-            <Card source={freddy} alternate="Freddy Krueger"/>
-            <Card source={michael} alternate="Michael Myers"/>
-            <Card source={chucky} alternate="Chucky"/>
-            <Card source={leather} alternate="Leatherface"/>
-            <Card source={xeno} alternate="Xenomorph"/>
-            <Card source={predator} alternate="Predator"/>
-            <Card source={pinhead} alternate="Pinhead"/>
+        <fieldset className="gameboard">
+            <Card function={RandomImagesArray} source={images[0][1]} alternate={images[0][0]}/>   
+            <Card function={RandomImagesArray} source={images[1][1]} alternate={images[1][0]}/>
+            <Card function={RandomImagesArray} source={images[2][1]} alternate={images[2][0]}/>
+            <Card function={RandomImagesArray} source={images[3][1]} alternate={images[3][0]}/>
+            <Card function={RandomImagesArray} source={images[4][1]} alternate={images[4][0]}/>
+            <Card function={RandomImagesArray} source={images[5][1]} alternate={images[5][0]}/>
+            <Card function={RandomImagesArray} source={images[6][1]} alternate={images[6][0]}/>
+            <Card function={RandomImagesArray} source={images[7][1]} alternate={images[7][0]}/>
+            <Card function={RandomImagesArray} source={images[8][1]} alternate={images[8][0]}/>
+            <Card function={RandomImagesArray} source={images[9][1]} alternate={images[9][0]}/>
+            <Card function={RandomImagesArray} source={images[10][1]} alternate={images[10][0]}/>
+            <Card function={RandomImagesArray} source={images[11][1]} alternate={images[11][0]}/>
+            <Card function={RandomImagesArray} source={images[12][1]} alternate={images[12][0]}/>
+            <Card function={RandomImagesArray} source={images[13][1]} alternate={images[13][0]}/>
+            <Card function={RandomImagesArray} source={images[14][1]} alternate={images[14][0]}/>
+            <Card function={RandomImagesArray} source={images[15][1]} alternate={images[15][0]}/>
         </fieldset>
         </>
     )
