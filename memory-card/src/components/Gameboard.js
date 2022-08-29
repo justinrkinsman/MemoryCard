9a111d25f39a6225a4b38180a1b55ed3759ac169
Justin <justinrkinsman@gmail.com>
2022-08-29 10:50:04 -0300

    Add function to gameboard.js to determine if card has already been clicked or not

--- memory-card/src/components/Gameboard.js
+++ memory-card/src/components/Gameboard.js
@@ -18,27 +18,29 @@ import xeno from "../images/xenomorph.jpg"
 import predator from "../images/predator.jpg"
 import pinhead from "../images/pinhead.jpg"
 
-const images = [
-    ['Dracula', dracula], 
-    ['Frankenstein', frankenstein], 
-    ['Nosferatu', nosferatu],
-    ['The Phantom of the Opera', phantom], 
-    ['The Mummy', mummy],
-    ['The Invisible Man', invisible], 
-    ['The Wolf Man', wolfman],
-    ['The Creature From the Black Lagoon', creature], 
-    ['Jason Voorhees', jason],
-    ['Freddy Krueger', freddy], 
-    ['Michael Myers', michael], 
-    ['Chucky', chucky],
-    ['Leatherface', leather], 
-    ['Xenomorph', xeno],
-    ['Predator', predator], 
-    ['Pinhead', pinhead]
-]
-
 export function Gameboard() {
 
+    const [clickedCard, setClickedCard] = useState([])
+    const [currentScore, setCurrentScore] = useState(0)
+    const [HighScore, setHighScore] = useState(0)
+    const [images, setImages] = useState([
+        ['Dracula', dracula], 
+        ['Frankenstein', frankenstein], 
+        ['Nosferatu', nosferatu],
+        ['The Phantom of the Opera', phantom], 
+        ['The Mummy', mummy],
+        ['The Invisible Man', invisible], 
+        ['The Wolf Man', wolfman],
+        ['The Creature From the Black Lagoon', creature], 
+        ['Jason Voorhees', jason],
+        ['Freddy Krueger', freddy], 
+        ['Michael Myers', michael], 
+        ['Chucky', chucky],
+        ['Leatherface', leather], 
+        ['Xenomorph', xeno],
+        ['Predator', predator], 
+        ['Pinhead', pinhead]
+    ])
 
     const RandomImagesArray = () => {
         let currentIndex = images.length
@@ -53,25 +55,34 @@ export function Gameboard() {
 
     RandomImagesArray()
 
+    const PlayerChoosesCard = (e) => {
+        if(clickedCard.includes(e.target.alt) === false){
+            setClickedCard(current => [...current, e.target.alt])
+        }else{
+            setClickedCard([])
+        }
+        console.log(clickedCard)
+    }
+    
     return(
         <>
         <fieldset className="gameboard">
-            <Card function={RandomImagesArray} source={images[0][1]} alternate={images[0][0]}/>   
-            <Card function={RandomImagesArray} source={images[1][1]} alternate={images[1][0]}/>
-            <Card function={RandomImagesArray} source={images[2][1]} alternate={images[2][0]}/>
-            <Card function={RandomImagesArray} source={images[3][1]} alternate={images[3][0]}/>
-            <Card function={RandomImagesArray} source={images[4][1]} alternate={images[4][0]}/>
-            <Card function={RandomImagesArray} source={images[5][1]} alternate={images[5][0]}/>
-            <Card function={RandomImagesArray} source={images[6][1]} alternate={images[6][0]}/>
-            <Card function={RandomImagesArray} source={images[7][1]} alternate={images[7][0]}/>
-            <Card function={RandomImagesArray} source={images[8][1]} alternate={images[8][0]}/>
-            <Card function={RandomImagesArray} source={images[9][1]} alternate={images[9][0]}/>
-            <Card function={RandomImagesArray} source={images[10][1]} alternate={images[10][0]}/>
-            <Card function={RandomImagesArray} source={images[11][1]} alternate={images[11][0]}/>
-            <Card function={RandomImagesArray} source={images[12][1]} alternate={images[12][0]}/>
-            <Card function={RandomImagesArray} source={images[13][1]} alternate={images[13][0]}/>
-            <Card function={RandomImagesArray} source={images[14][1]} alternate={images[14][0]}/>
-            <Card function={RandomImagesArray} source={images[15][1]} alternate={images[15][0]}/>
+            <Card function={PlayerChoosesCard} source={images[0][1]} alternate={images[0][0]}/>   
+            <Card function={PlayerChoosesCard} source={images[1][1]} alternate={images[1][0]}/>
+            <Card function={PlayerChoosesCard} source={images[2][1]} alternate={images[2][0]}/>
+            <Card function={PlayerChoosesCard} source={images[3][1]} alternate={images[3][0]}/>
+            <Card function={PlayerChoosesCard} source={images[4][1]} alternate={images[4][0]}/>
+            <Card function={PlayerChoosesCard} source={images[5][1]} alternate={images[5][0]}/>
+            <Card function={PlayerChoosesCard} source={images[6][1]} alternate={images[6][0]}/>
+            <Card function={PlayerChoosesCard} source={images[7][1]} alternate={images[7][0]}/>
+            <Card function={PlayerChoosesCard} source={images[8][1]} alternate={images[8][0]}/>
+            <Card function={PlayerChoosesCard} source={images[9][1]} alternate={images[9][0]}/>
+            <Card function={PlayerChoosesCard} source={images[10][1]} alternate={images[10][0]}/>
+            <Card function={PlayerChoosesCard} source={images[11][1]} alternate={images[11][0]}/>
+            <Card function={PlayerChoosesCard} source={images[12][1]} alternate={images[12][0]}/>
+            <Card function={PlayerChoosesCard} source={images[13][1]} alternate={images[13][0]}/>
+            <Card function={PlayerChoosesCard} source={images[14][1]} alternate={images[14][0]}/>
+            <Card function={PlayerChoosesCard} source={images[15][1]} alternate={images[15][0]}/>
         </fieldset>
         </>
     )
