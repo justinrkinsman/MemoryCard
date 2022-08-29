7beafdee4271d296ce6e3c6bababe6bdd3ef4261
Justin <justinrkinsman@gmail.com>
2022-08-29 14:02:20 -0300

    Clean up commented code

--- memory-card/src/components/Card.js
+++ memory-card/src/components/Card.js
@@ -1,53 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import "./card.css"
-{/*import dracula from "../images/dracula.jpg"
-import frankenstein from "../images/frankenstein.jpg"
-import nosferatu from "../images/nosferatu.jpg"
-import phantom from "../images/phantom.jpg"
-import mummy from "../images/mummy.jpg"
-import invisible from "../images/invisibleman.jpg"
-import wolfman from "../images/wolfman.jpg"
-import creature from "../images/creature.jpg"
-import jason from "../images/jason.jpg"
-import freddy from "../images/freddy.jpg"
-import michael from "../images/myers.jpg"
-import chucky from "../images/chucky.jpg"
-import leather from "../images/leatherface.jpg"
-import xeno from "../images/xenomorph.jpg"
-import predator from "../images/predator.jpg"
-import pinhead from "../images/pinhead.jpg"
-
-const images = [
-    ['Dracula', dracula], 
-    ['Frankenstein', frankenstein], 
-    ['Nosferatu', nosferatu],
-    ['The Phantom of the Opera', phantom], 
-    ['The Mummy', mummy],
-    ['The Invisible Man', invisible], 
-    ['The Wolf Man', wolfman],
-    ['The Creature From the Black Lagoon', creature], 
-    ['Jason Voorhees', jason],
-    ['Freddy Krueger', freddy], 
-    ['Michael Myers', michael], 
-    ['Chucky', chucky],
-    ['Leatherface', leather], 
-    ['Xenomorph', xeno],
-    ['Predator', predator], 
-    ['Pinhead', pinhead]
-]*/}
 
 export function Card(props) {
-
-    {/*const RandomImagesArray = () => {
-        let currentIndex = images.length
-        while (currentIndex !== 0){
-            let randomIndex = Math.floor(Math.random() * currentIndex)
-            currentIndex--
-
-            [images[currentIndex], images[randomIndex]] = [images[randomIndex], images[currentIndex]]
-        }
-        console.log(images)
-    }*/}
 
     return(
         <>
